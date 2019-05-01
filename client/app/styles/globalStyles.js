98bf057c2cb7b5ea595e76a7b37d25a0eaa51337
Aakash Goel <agoel36@sapient.com>
2019-05-01 19:56:14 +0530

    chore: 🏗  update global styles

--- client/app/styles/globalStyles.js
+++ client/app/styles/globalStyles.js
@@ -1,7 +1,28 @@
-import { defaultFont } from './theme/muiKit';
+import {
+  defaultFont,
+  primaryFontFamily,
+  secondaryFontFamily,
+} from './theme/muiKit';
 
 export default {
   '@global': {
-    body: defaultFont,
+    html: {
+      height: '100%',
+    },
+    body: {
+      ...defaultFont,
+      height: '100%',
+      background: '#e6ecf0',
+      color: '#14171a',
+    },
+    'h1, h2, h3, h4': {
+      fontFamily: secondaryFontFamily,
+      fontWeight: 500,
+      margin: 0,
+    },
+    'h5, h6': {
+      fontFamily: primaryFontFamily,
+      fontWeight: 400,
+    },
   },
 };
