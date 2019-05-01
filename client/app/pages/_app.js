953c94b747244b771634ae48963dfd8305edbeeb
Aakash Goel <agoel36@sapient.com>
2019-05-01 19:52:47 +0530

    fix: 🏗  duplicate style creation issue

--- client/app/pages/_app.js
+++ client/app/pages/_app.js
@@ -17,7 +17,13 @@ import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
 import withReduxSaga from 'next-redux-saga';
 
+import JssProvider from 'react-jss/lib/JssProvider';
+import { MuiThemeProvider, withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import getPageContext from '../utils/getPageContext';
 import createStore from '../store/store';
+import globalStyles from '../styles/globalStyles';
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -29,16 +35,47 @@ class MyApp extends App {
     return { pageProps };
   }
 
+  constructor() {
+    super();
+    this.pageContext = getPageContext();
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const { Component, pageProps, store } = this.props;
     return (
       <Container>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
+        <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+          <MuiThemeProvider
+            theme={this.pageContext.theme}
+            sheetsManager={this.pageContext.sheetsManager}
+          >
+            <Provider store={store}>
+              {/* CssBaseline kick start an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              {/* Pass pageContext to the _document though the renderPage enhancer
+                    to render collected styles on server-side. */}
+              <Component pageContext={this.pageContext} {...pageProps} />
+            </Provider>
+          </MuiThemeProvider>
+        </JssProvider>
       </Container>
     );
   }
 }
 
-export default withRedux(createStore)(withReduxSaga({ async: true })(MyApp));
+export default withRedux(createStore)(
+  withReduxSaga({ async: true })(withStyles(globalStyles)(MyApp))
+);
