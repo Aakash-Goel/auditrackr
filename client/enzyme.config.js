ffe1c36fa5a41451e00888b111bb05f621bab307
Aakash Goel <agoel36@sapient.com>
2019-05-04 18:34:58 +0530

    feat(carousel): 🎉 initial commit of new feaature carousel

--- client/enzyme.config.js
+++ client/enzyme.config.js
@@ -3,3 +3,15 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+/* eslint-disable func-names */
+
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {},
+    };
+  };
